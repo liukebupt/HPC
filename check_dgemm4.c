5195e704b029136c3f737f7bd2989ac94226b3a3
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-04 20:58:56 -0700

    Update and rename dgemm4.c to check_dgemm4.c

--- check_dgemm4.c
+++ check_dgemm4.c
@@ -32,13 +32,17 @@ int main(int argc, char* argv[]) {
 
   for (i=0;i<n;i+=2)
     for (j=0;j<n;j+=2) {
-      register double c00=c3[i*n+j], c01=c3[i*n+j+1], c10=c3[(i+1)*n+j], c11=c3[(i+1)*n+j+1];
+      register double c00=0, c01=0, c10=0, c11=0;
       for (k=0;k<n;k++) {
         c00 += a[i*n+k]*b[k*n+j];
         c10 += a[(i+1)*n+k]*b[k*n+j];
         c01 += a[i*n+k]*b[k*n+j+1];
         c11 += a[(i+1)*n+k]*b[k*n+j+1];
       }
+      c3[i*n+j]=c00;
+      c3[i*n+j+1]=c10;
+      c3[(i+1)*n+j]=c01;
+      c3[(i+1)*n+j+1]=c11;
     }
   
   double max_diff=0,cur_diff;
