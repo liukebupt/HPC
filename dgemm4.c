6ddc0403f449ede1fc8a8aefbca9dc147332531d
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-04 21:40:22 -0700

    Update dgemm4.c

--- dgemm4.c
+++ dgemm4.c
@@ -31,14 +31,13 @@ int main(int argc, char* argv[]) {
   
   register int j,k;
 
-  register int pc1=0, pc2=n;
-
   for (i=0;i<n;i+=2) {
     for (j=0;j<n;j+=4) {
+      register int pc1=i*n+j, pc2=pc1+n;
       register double c00=0, c01=0, c02=0, c03=0, c10=0, c11=0, c12=0, c13=0;
-      register int pa1=i*n, pa2=pa1+n, pb=j;
       for (k=0;k<n;k++) {
-	       register double a0=a[pa1], a1=a[pa2], b0=b[pb], b1=b[pb+1], b2=b[pb+2], b3=b[pb+3];
+	register int pa1=i*n+k, pa2=pa1+n, pb=k*n+j;
+	register double a0=a[pa1], a1=a[pa2], b0=b[pb], b1=b[pb+1], b2=b[pb+2], b3=b[pb+3];
         c00 += a0*b0;
         c01 += a0*b1;
         c02 += a0*b2;
@@ -47,9 +46,6 @@ int main(int argc, char* argv[]) {
         c11 += a1*b1;
         c12 += a1*b2;
         c13 += a1*b3;
-        pa1+=1;
-        pa2+=1;
-        pb+=n;
       }
       c[pc1]=c00;
       c[pc1+1]=c01;
@@ -59,12 +55,7 @@ int main(int argc, char* argv[]) {
       c[pc2+1]=c11;
       c[pc2+2]=c12;
       c[pc2+3]=c13;
-      pc1+=4;
-      pc2+=4;
     }
-    pc1+=n;
-    pc2+=n;
-  }  
 
   printf("time passed %.2f seconds.\n",(double)(clock()-start)/CLOCKS_PER_SEC);
 
