a4fda1243d940092d58d5c5402d3916e569e5301
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-04 21:54:55 -0700

    Update dgemm4.c

--- dgemm4.c
+++ dgemm4.c
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
 
   //printf("RAND_MAX=%d, CLOCKS_PER_SEC=%d.\n",RAND_MAX,CLOCKS_PER_SEC);
 
-  int n=atoi(argv[1]);
+  register int n=atoi(argv[1]);
 
   printf("Testing dgemm3 with n=%d.\n",n);
 
@@ -20,7 +20,7 @@ int main(int argc, char* argv[]) {
   double *b=(double *)malloc(sizeof(double)*n*n);
   double *c=(double *)calloc(sizeof(double),n*n);
 
-  int i;
+  register int i;
 
   for (i=0; i<n*n; i++) {
     a[i]=drand();
@@ -29,14 +29,15 @@ int main(int argc, char* argv[]) {
   
   clock_t start=clock();
   
-  int j,k;
+  register int j,k;
 
-  for (i=0;i<n;i+=2)
+  for (i=0;i<n;i+=2) {
+    register int pc1=i*n;
     for (j=0;j<n;j+=4) {
-      register int pc1=i*n+j, pc2=pc1+n;
+      register int pc2=pc1+n, pa1=i*n, pb=j;
       register double c00=0, c01=0, c02=0, c03=0, c10=0, c11=0, c12=0, c13=0;
       for (k=0;k<n;k++) {
-	register int pa1=i*n+k, pa2=pa1+n, pb=k*n+j;
+	register int pa2=pa1+n;
 	register double a0=a[pa1], a1=a[pa2], b0=b[pb], b1=b[pb+1], b2=b[pb+2], b3=b[pb+3];
         c00 += a0*b0;
         c01 += a0*b1;
@@ -46,6 +47,8 @@ int main(int argc, char* argv[]) {
         c11 += a1*b1;
         c12 += a1*b2;
         c13 += a1*b3;
+	pa1+=1;
+	pb+=n;
       }
       c[pc1]=c00;
       c[pc1+1]=c01;
@@ -55,7 +58,9 @@ int main(int argc, char* argv[]) {
       c[pc2+1]=c11;
       c[pc2+2]=c12;
       c[pc2+3]=c13;
+      pc1+=1;
     }
+  }
 
   printf("time passed %.2f seconds.\n",(double)(clock()-start)/CLOCKS_PER_SEC);
 
