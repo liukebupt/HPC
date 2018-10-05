25e9ec4992432236a2aac04909fb39f012e899b1
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-04 22:02:18 -0700

    Update check_dgemm4.c

--- check_dgemm4.c
+++ check_dgemm4.c
@@ -30,20 +30,36 @@ int main(int argc, char* argv[]) {
   
   int j,k;
 
-  for (i=0;i<n;i+=2)
-    for (j=0;j<n;j+=2) {
-      register double c00=0, c01=0, c10=0, c11=0;
+  for (i=0;i<n;i+=2) {
+    register int pc1=i*n;
+    for (j=0;j<n;j+=4) {
+      register int pc2=pc1+n, pa1=i*n, pb=j;
+      register double c00=0, c01=0, c02=0, c03=0, c10=0, c11=0, c12=0, c13=0;
       for (k=0;k<n;k++) {
-        c00 += a[i*n+k]*b[k*n+j];
-        c10 += a[(i+1)*n+k]*b[k*n+j];
-        c01 += a[i*n+k]*b[k*n+j+1];
-        c11 += a[(i+1)*n+k]*b[k*n+j+1];
+	       register int pa2=pa1+n;
+	       register double a0=a[pa1], a1=a[pa2], b0=b[pb], b1=b[pb+1], b2=b[pb+2], b3=b[pb+3];
+        c00 += a0*b0;
+        c01 += a0*b1;
+        c02 += a0*b2;
+        c03 += a0*b3;
+        c10 += a1*b0;
+        c11 += a1*b1;
+        c12 += a1*b2;
+        c13 += a1*b3;
+	       pa1+=1;
+	       pb+=n;
       }
-      c3[i*n+j]=c00;
-      c3[i*n+j+1]=c01;
-      c3[(i+1)*n+j]=c10;
-      c3[(i+1)*n+j+1]=c11;
+      c3[pc1]=c00;
+      c3[pc1+1]=c01;
+      c3[pc1+2]=c02;
+      c3[pc1+3]=c03;
+      c3[pc2]=c10;
+      c3[pc2+1]=c11;
+      c3[pc2+2]=c12;
+      c3[pc2+3]=c13;
+      pc1+=1;
     }
+  }
   
   double max_diff=0,cur_diff;
 
