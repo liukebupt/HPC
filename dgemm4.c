62e16cf9d49976db86612508872d98d1a6e37d5d
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-04 21:30:41 -0700

    Update dgemm4.c

--- dgemm4.c
+++ dgemm4.c
@@ -12,15 +12,15 @@ int main(int argc, char* argv[]) {
 
   //printf("RAND_MAX=%d, CLOCKS_PER_SEC=%d.\n",RAND_MAX,CLOCKS_PER_SEC);
 
-  int n=atoi(argv[1]);
+  register int n=atoi(argv[1]);
 
-  printf("Testing dgemm4 with n=%d.\n",n);
+  printf("Testing dgemm3 with n=%d.\n",n);
 
   double *a=(double *)malloc(sizeof(double)*n*n);
   double *b=(double *)malloc(sizeof(double)*n*n);
   double *c=(double *)calloc(sizeof(double),n*n);
 
-  int i;
+  register int i;
 
   for (i=0; i<n*n; i++) {
     a[i]=drand();
@@ -29,23 +29,43 @@ int main(int argc, char* argv[]) {
   
   clock_t start=clock();
   
-  int j,k;
+  register int j,k;
 
-  for (i=0;i<n;i+=2)
-    for (j=0;j<n;j+=2) {
-      register double c00=0, c01=0, c10=0, c11=0;
+  register int pc1=0, pc2=n;
+
+  for (i=0;i<n;i+=2) {
+    for (j=0;j<n;j+=4) {
+      register double c00=0, c01=0, c02=0, c03=0, c10=0, c11=0, c12=0, c13=0;
+      register int pa1=i*n, pa2=pa1+n, pb=j;
       for (k=0;k<n;k++) {
-        c00 += a[i*n+k]*b[k*n+j];
-        c10 += a[(i+1)*n+k]*b[k*n+j];
-        c01 += a[i*n+k]*b[k*n+j+1];
-        c11 += a[(i+1)*n+k]*b[k*n+j+1];
+	       register double a0=a[pa1], a1=a[pa2], b0=b[pb], b1=b[pb+1], b2=b[pb+2], b3=b[pb+3];
+        c00 += a0*b0;
+        c01 += a0*b1;
+        c02 += a0*b2;
+        c03 += a0*b3;
+        c10 += a1*b0;
+        c11 += a1*b1;
+        c12 += a1*b2;
+        c13 += a1*b3;
+        pa1+=1;
+        pa2+=1;
+        pb+=n;
       }
-      c[i*n+j]=c00;
-      c[i*n+j+1]=c01;
-      c[(i+1)*n+j]=c10;
-      c[(i+1)*n+j+1]=c11;
+      c[pc1]=c00;
+      c[pc1+1]=c01;
+      c[pc1+2]=c02;
+      c[pc1+3]=c03;
+      c[pc2]=c10;
+      c[pc2+1]=c11;
+      c[pc2+2]=c12;
+      c[pc2+3]=c13;
+      pc1+=4;
+      pc2+=4;
     }
-    
+    pc1+=n;
+    pc2+=n;
+  }  
+
   printf("time passed %.2f seconds.\n",(double)(clock()-start)/CLOCKS_PER_SEC);
 
   free(a);
