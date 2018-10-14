b14e252993b4c7359e1c83274c4b6cd2eae36590
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-14 01:18:31 -0700

    Update lapack_approach_test.c

--- lapack_approach_test.c
+++ lapack_approach_test.c
@@ -5,8 +5,15 @@
 
 int main (int argc, const char * argv[]) {
 
-  double A[2][2] = {3,4,5,9};
-  double B[2][1] = {5,3};
+  double *A= (double *)malloc(sizeof(double)*4);
+  A[0]=3;
+  A[1]=4;
+  A[2]=5;
+  A[3]=9;
+  
+  double *B= (double *)malloc(sizeof(double)*2);
+  B[0]=5;
+  B[1]=3;
 
   lapack_int info,m,n=2,lda,ldb,nrhs;
   lapack_int *ipiv;
@@ -17,9 +24,9 @@ int main (int argc, const char * argv[]) {
   {
      for(j=0;j<n;j++)
      {
-        printf("%lf ",A[i][j]);
+        printf("%lf ",A[i*n+j]);
      }
-     printf("  %lf ", B[i][0]);
+     printf("  %lf ", B[i]);
      printf("\n");
   }
  
@@ -36,7 +43,7 @@ int main (int argc, const char * argv[]) {
   {
      for(j=0;j<n;j++)
      {
-        printf("%lf ",A[i][j]);
+        printf("%lf ",A[i*n+j]);
      }
      printf("\n");
   }
@@ -52,19 +59,19 @@ int main (int argc, const char * argv[]) {
     }
   }
   
-  cblas_dtrsm(LAPACK_ROW_MAJOR, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, n, n, 1, *A, n, *B, n);
+  cblas_dtrsm(LAPACK_ROW_MAJOR, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, n, n, 1, A, n, B, n);
   printf("y:\n");
   for(i=0;i<n;i++)
   {
-     printf("  %lf ", B[i][0]);
+     printf("  %lf ", B[i]);
      printf("\n");
   }
-  cblas_dtrsm(LAPACK_ROW_MAJOR, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, n, n, 1, *A, n, *B, n);
+  cblas_dtrsm(LAPACK_ROW_MAJOR, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, n, n, 1, A, n, B, n);
   
   printf("Result:\n");
   for(i=0;i<n;i++)
   {
-     printf("  %lf ", B[i][0]);
+     printf("  %lf ", B[i]);
      printf("\n");
   }
   return 0;
