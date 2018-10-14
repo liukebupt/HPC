f66f0c9896349bb8da83b99c161c344ca74c5511
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-13 17:25:23 -0700

    Update lapack_approach_test.c

--- lapack_approach_test.c
+++ lapack_approach_test.c
@@ -1,27 +1,56 @@
 #include <stdio.h>
+#include "cblas.h"
 #include <lapacke.h>
+#include <string.h>
 
 int main (int argc, const char * argv[]) {
-  
+
   double A[2][2] = {3,4,5,9};
   double B[2][1] = {5,3};
-   
-  lapack_int n=2,*ipiv;
-  
+
+  lapack_int info,m,n,lda,ldb,nrhs;
+  lapack_int *ipiv;
+  int i,j;
 
   n = 2;
   ipiv = (lapack_int *)malloc(n*sizeof(lapack_int));
+  m = n;
+  nrhs = 2;
+  lda = m;
+  ldb = 2;
 
-  LAPACKE_dgetrf(LAPACK_ROW_MAJOR, n, n, *A, n, ipiv);
+  LAPACKE_dgetrf(LAPACK_ROW_MAJOR, m, n, *A, lda, ipiv);
+  double *temp=malloc(n);
+
+  for (i=0;i<n;i++)
+  {
+    ipiv[i]--;
+    if (ipiv[i]!=i) {
+      memcpy(temp,A[i],n);
+      memcpy(A[i],A[ipiv[i]],n);
+      memcpy(A[ipiv[i]],temp,n);
+    }
+  }
+
+  for(i=0;i<n;i++)
+  {
+    for(j=0;j<n;j++)
+    {
+      printf("%lf ",A[i][j]);
+    }
+    printf("  %lf ", B[i][0]);
+    printf("\n");
+  }
   
-  int i,j;
+  cblas_dtrsm(LAPACK_ROW_MAJOR, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, n, 1, 1, *A, n, *B, n);
+
   for(i=0;i<n;i++)
   {
      for(j=0;j<n;j++)
      {
         printf("%lf ",A[i][j]);
      }
-     printf("  %6i ", ipiv[i]);
+     printf("  %lf ", B[i][0]);
      printf("\n");
   }
   return 0;
