cf5eaa0bdb1c65893c875e6f448cddb244fa9ef0
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-14 23:25:19 -0700

    Update test_compare.c

--- test_compare.c
+++ test_compare.c
@@ -71,7 +71,7 @@ int main (int argc, const char * argv[]) {
   for (i=0;i<n;i++) {
     for (j=0;j<n;j++) 
       printf("%f ",A[i*n+j]);
-    printf("\n");
+    printf("%d \n",pvt[i]);
   }
   y[0]=b[pvt[0]];
   for (i=1;i<n;i++) {
