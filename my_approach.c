dee5efd290ef9498e31ef43909dc8c8a8d1e15b9
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-14 18:00:14 -0700

    Update my_approach.c

--- my_approach.c
+++ my_approach.c
@@ -74,7 +74,7 @@ int main (int argc, const char * argv[]) {
   
   double x[2];
   x[n-1]=y[n-1]/A[n-1][n-1];
-  for (i=n-1;i>0;--i) {
+  for (i=n-1;i>-1;i--) {
     sum=0;
     for (j=i+1;j<n;j++)
       sum+=x[j]*A[i][j];
