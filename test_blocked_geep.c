8ae5d99b926915993ce7dd461fb1feac74dbc3f6
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-16 18:51:19 -0700

    Update test_blocked_geep.c

--- test_blocked_geep.c
+++ test_blocked_geep.c
@@ -15,7 +15,7 @@ int main (int argc, const char * argv[]) {
   for (i=0; i<n; i++)
     for (j=0; j<n; j++) 
       A[i*n+j]=drand();
-  printf("input LU:\n");
+  printf("input:\n");
   for(i=0;i<n;i++)
   {
      for(j=0;j<n;j++)
