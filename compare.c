6594fc093d4c3a6e3df34cdb5872ae009cca49b3
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-20 00:10:33 -0700

    Update compare.c

--- compare.c
+++ compare.c
@@ -3,9 +3,10 @@
 #include <time.h>
 #include <stdbool.h>
 #include <cblas.h>
+#include <math.h>
 #include <string.h>
 
-#define drand() (double)rand()/RAND_MAX+1 //return a random double number between 1 and 2.
+#define drand() (double)rand()/RAND_MAX*(-2)+1 //return a random double number between -1 and 1.
 
 int main (int argc, const char * argv[]) {
 
@@ -14,24 +15,25 @@ int main (int argc, const char * argv[]) {
     return 0;
   }
   int n=atoi(argv[1]);
-  printf("n=.\n",n);
+  printf("n=%d. \n",n);
  
   bool test=false;
-  if (argv[2]=="True")
+  if (argv[2][0]=='T')
     test=true;
 
   double *A=(double *)malloc(sizeof(double)*n*n);
+  double *A_bak=(double *)malloc(sizeof(double)*n*n);
   double *b=(double *)malloc(sizeof(double)*n);
   int i, j, k;
   for (i=0; i<n; i++) {
-    for (j=0; j<n; j++) {
+    for (j=0; j<n; j++) 
       A[i*n+j]=drand();
-    }
     b[i]=drand();
   }  
+  memcpy(A_bak,A,sizeof(double)*n*n);
   
-  int temps, maxind, max;
-  double sum;
+  int temps, maxind;
+  double sum, max;
   double *tempv = (double *)malloc(sizeof(double)*n);
   double *y = (double *)malloc(sizeof(double)*n);
   double *x = (double *)malloc(sizeof(double)*n);
@@ -42,24 +44,24 @@ int main (int argc, const char * argv[]) {
     pvt[i]=i;
   for (i=0;i<n-1;i++) {
     maxind=i;
-    max=abs(A[i*n+i]);
+    max=fabs(A[i*n+i]);
     for (j=i+1;j<n;j++) {
-      if (abs(A[j*n+i])>max) {
+      if (fabs(A[j*n+i])>max) {
         maxind = j;
-        max = abs(A[j*n+i]);
+        max = fabs(A[j*n+i]);
       }
     }
     if (max==0) {
-      printf("LUfactoration failed: coefficient matrix is singular");
+      printf("LU factoration failed: coefficient matrix is singular\n\n");
       return -1;
     } else {
       if (maxind!=i) {
         temps=pvt[i];
         pvt[i]=pvt[maxind];
         pvt[maxind]=temps;
-        memcpy(tempv,&A[i],sizeof(double)*n);
-        memcpy(&A[i],&A[maxind],sizeof(double)*n);
-        memcpy(&A[maxind],tempv,sizeof(double)*n); 
+        memcpy(tempv,&A[i*n],sizeof(double)*n);
+        memcpy(&A[i*n],&A[maxind*n],sizeof(double)*n);
+        memcpy(&A[maxind*n],tempv,sizeof(double)*n); 
       }
     }
     for (j=i+1;j<n;j++) {
@@ -88,9 +90,8 @@ int main (int argc, const char * argv[]) {
   double temp;
   
   start=clock();
-  LAPACKE_dgetrf(LAPACK_ROW_MAJOR, n, n, A, n, ipiv);
-  for (i=0;i<n;i++)
-  {
+  LAPACKE_dgetrf(LAPACK_ROW_MAJOR, n, n, A_bak, n, ipiv);
+  for (i=n-1;i>-1;i--) {
     ipiv[i]--;
     if (ipiv[i]!=i) {
       temp=b[i];
@@ -98,23 +99,24 @@ int main (int argc, const char * argv[]) {
       b[ipiv[i]]=temp;
     }
   }
-  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, n, 1, 1, A, n, b, 1);
-  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, n, 1, 1, A, n, b, 1);
+  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasLower, CblasNoTrans, CblasUnit, n, 1, 1, A_bak, n, b, 1);
+  cblas_dtrsm(CblasRowMajor, CblasLeft, CblasUpper, CblasNoTrans, CblasNonUnit, n, 1, 1, A_bak, n, b, 1);
   printf("Cost %.2f seconds by LAPACKE's approach.\n",(double)(clock()-start)/CLOCKS_PER_SEC);
   
   if (test) {
     double max_diff=0, cur_diff;
     for (i=0;i<n;i++) {
-      cur_diff=abs(x[i]-b[i]);
+      cur_diff=fabs(x[i]-b[i]);
       if (cur_diff>max_diff)
         max_diff=cur_diff;
     }
-    printf("The maximum difference between LAPACKE's approach and mine is %f.\n", max_diff);
+    printf("The maximum difference between LAPACKE's approach and mine is %.16f.\n", max_diff);
   }
   
   printf("\n");
 
   free(A);
+  free(A_bak);
   free(b);
   free(tempv);
   free(x);
