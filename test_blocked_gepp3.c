c7d8d4edebf33e7051da3b43f93863526ff0470c
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-19 23:53:32 -0700

    Create test_blocked_gepp3.c

---
+++ test_blocked_gepp3.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <lapacke.h>
+#include <cblas.h>
+#include <time.h>
+#include <stdbool.h>
+
+#define drand() (double)rand()/RAND_MAX*(-2)+1 //return a random double number between -1 and 1.
+
+int main (int argc, const char * argv[]) {
+  
+  if (argc!=4) {
+    printf("Invalid input!\n");
+    return 0;
+  }
+  int n=atoi(argv[1]);
+  int B=atoi(argv[2]);
+  printf("Testing blocked gepp with n=%d, B=%d.\n", n, B);
+  
+  bool test=false;
+  if (argv[3][0]=='T')
+    test=true;
+  
+  double *A=(double *)malloc(sizeof(double)*n*n);
+  double *b=(double *)malloc(sizeof(double)*n);
+  double *A_bak=(double *)malloc(sizeof(double)*n*n);
+  int i, j, k, l, j1, k1;
+  for (i=0; i<n; i++) {
+    for (j=0; j<n; j++) 
+      A[i*n+j]=drand();
+    b[i]=drand(); 
+  }
+  memcpy(A_bak,A,sizeof(double)*n*n);
+  
+  int temps, maxind, end;
+  double max, sum;
+  double *tempv = (double *)malloc(sizeof(double)*n);
+  double *y = (double *)malloc(sizeof(double)*n);
+  double *x = (double *)malloc(sizeof(double)*n);
+  
+  int *pvt = (int *)malloc(sizeof(int)*n);
+
+  clock_t start=clock();
+  for (i=0;i<n;i++)
+    pvt[i]=i;
+  for (i=0;i<n;i+=B) {
+    end=i+B;
+    for (j=i;j<end;j++) {
+      maxind=j;
+      max=fabs(A[j*n+j]);
+      for (k=j+1;k<n;k++) {
+        if (fabs(A[k*n+j])>max) {
+          maxind = k;
+          max = fabs(A[k*n+j]);
+        }
+      }
+      if (max==0) {
+        printf("LU factoration failed: coefficient matrix is singular\n\n");
+        return -1;
+      } else {
+        if (maxind!=j) {
+          temps=pvt[j];
+          pvt[j]=pvt[maxind];
+          pvt[maxind]=temps;
+          memcpy(tempv,&A[j*n],sizeof(double)*n);
+          memcpy(&A[j*n],&A[maxind*n],sizeof(double)*n);
+          memcpy(&A[maxind*n],tempv,sizeof(double)*n); 
+        }
+      }
+      for (k=j+1;k<n;k++) {
+        A[k*n+j]=A[k*n+j]/A[j*n+j];
+        for (l=j+1;l<end;l++)
+          A[k*n+l]=A[k*n+l]-A[k*n+j]*A[j*n+l];
+      }
+    }
+    for (j=i;j<end;j++)
+      for (k=j+1;k<end;k++)
+        for (l=end;l<n;l++)
+          A[k*n+l]-=A[k*n+j]*A[j*n+l];
+    for (j=end;j<n;j+=B)
+      for (k=end;k<n;k+=B)
+        for (j1=j;j1<j+B;j1++)
+          for (k1=k;k1<k+B;k1++) {
+            register double a=A[j1*n+k1];
+            for (l=i;l<end;l++)
+              a-=A[j1*n+l]*A[l*n+k1];
+            A[j1*n+k1]=a;
+          }
+  }
+  printf("Blocked LU:\n\n");
+  for (i=0;i<n;i++) {
+    for (j=0;j<n;j++) {
+      printf("%f\t", A[i*n+j]);
+    }
+    printf("%d\n\n", pvt[i]);
+  }
+  
+  memcpy(A,A_bak,sizeof(double)*n*n);
+  for (i=0;i<n;i++)
+    pvt[i]=i;
+  for (i=0;i<n-1;i++) {
+    maxind=i;
+    max=fabs(A[i*n+i]);
+    for (j=i+1;j<n;j++) {
+      if (fabs(A[j*n+i])>max) {
+        maxind = j;
+        max = fabs(A[j*n+i]);
+      }
+    }
+    if (max==0) {
+      printf("LU factoration failed: coefficient matrix is singular\n\n");
+      return -1;
+    } else {
+      if (maxind!=i) {
+        temps=pvt[i];
+        pvt[i]=pvt[maxind];
+        pvt[maxind]=temps;
+        memcpy(tempv,&A[i*n],sizeof(double)*n);
+        memcpy(&A[i*n],&A[maxind],sizeof(double)*n);
+        memcpy(&A[maxind*n],tempv,sizeof(double)*n); 
+      }
+    }
+    for (j=i+1;j<n;j++) {
+      A[j*n+i]=A[j*n+i]/A[i*n+i];
+      for (k=i+1;k<n;k++)
+        A[j*n+k]=A[j*n+k]-A[j*n+i]*A[i*n+k];
+    }
+  }
+  printf("Simple LU:\n\n");
+  for (i=0;i<n;i++) {
+    for (j=0;j<n;j++) {
+      printf("%f\t", A[i*n+j]);
+    }
+    printf("%d\n\n", pvt[i]);
+  }
+  
+  free(A_bak);
+  free(A);
+  free(b);
+  free(tempv);
+  free(pvt);
+  free(y);
+  free(x);
+
+  return 0;
+}
