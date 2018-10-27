b9afbd836f3e90fa793584787bc3939e306d973e
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-10-26 17:39:00 -0700

    Update sieve1.c

--- sieve1.c
+++ sieve1.c
@@ -6,17 +6,16 @@
 
 int main (int argc, char *argv[])
 {
-   ...
    MPI_Init (&argc, &argv);
    MPI_Barrier(MPI_COMM_WORLD);
    elapsed_time = -MPI_Wtime();
    MPI_Comm_rank (MPI_COMM_WORLD, &id);
    MPI_Comm_size (MPI_COMM_WORLD, &p);
-if (argc != 2) {
+   if (argc != 2) {
       if (!id) printf ("Command line: %s <m>\n", argv[0]);
       MPI_Finalize(); exit (1);
-}
-n = atoi(argv[1]);
+   }
+   n = atoi(argv[1]);
    low_value = 2 + BLOCK_LOW(id,p,n-1);
    high_value = 2 + BLOCK_HIGH(id,p,n-1);
    size = BLOCK_SIZE(id,p,n-1);
@@ -33,7 +32,7 @@ n = atoi(argv[1]);
       MPI_Finalize();
       exit (1);
    }
-for (i = 0; i < size; i++) marked[i] = 0;
+   for (i = 0; i < size; i++) marked[i] = 0;
    if (!id) index = 0;
    prime = 2;
    do {
@@ -53,12 +52,10 @@ for (i = 0; i < size; i++) marked[i] = 0;
    count = 0;
    for (i = 0; i < size; i++)
       if (!marked[i]) count++;
-   MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM,
-      0, MPI_COMM_WORLD);
+   MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
    elapsed_time += MPI_Wtime();
    if (!id) {
-      printf ("%d primes are less than or equal to %d\n",
-         global_count, n);
+      printf ("%d primes are less than or equal to %d\n", global_count, n);
       printf ("Total elapsed time: %10.6f\n", elapsed_time);
    }
    MPI_Finalize ();
