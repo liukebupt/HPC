847b9ec73e94a7a3b4c720ab2948bf39cb820985
liukebupt <34887912+liukebupt@users.noreply.github.com>
2018-11-01 15:40:25 -0700

    Update sieve3.c

--- sieve3.c
+++ sieve3.c
@@ -7,9 +7,9 @@
 
 int main (int argc, char *argv[])  
 {      
-  long long int n, low_value, high_value, Bsize, size, proc0_size, i, first, prime;
-  int id, p, index, count, global_count, j, B, k; 
-  char *marked, *pend;     
+  long long int n, low_value, Blow_value, Bsize, size, proc0_size, i, first, prime, gsize;
+  int id, p, index, count, global_count, j, B, k, sqn, psize, *newprimes, nprime; 
+  char *marked, *pend, *primes;     
   double elapsed_time;     
   MPI_Init (&argc, &argv); 
   MPI_Barrier(MPI_COMM_WORLD);    
@@ -22,22 +22,22 @@ int main (int argc, char *argv[])
     MPI_Finalize(); exit (1);    
   }   
   n = strtoll(argv[1], &pend, 10);  
+  gsize=n/2-1;
   B = atoi(argv[2]);
   
-  high_value = 3 + 2*BLOCK_HIGH(id,p,n/2-1);    
-  size = BLOCK_SIZE(id,p,n/2-1);  
-  proc0_size = (n/2-1)/p;  
-  if ((3 + 2*proc0_size) < (int) sqrt((double) n)) {   
+  low_value = 3 + 2*BLOCK_LOW(id,p,gsize);     
+  size = BLOCK_SIZE(id,p,gsize);  
+  proc0_size = gsize/p;  
+  sqn=(int) sqrt((double) n)
+  if ((3 + 2*proc0_size) < sqn) {   
     if (!id) printf ("Too many processes\n");  
     MPI_Finalize();
     exit (1);      
   }   
   //printf("id = %d p = %d n = %lld size = %" PRId64 "\n", id, p, n, size);     
 
-  int sqn=sqrt(n);
-  int psize=sqn/2-1;
-  char *primes = (char *) malloc (psize); 
-  for (i = 0; i < psize; i++) primes[i] = 0;  
+  psize=sqn/2;
+  primes = (char *) calloc (psize, sizeof(char)); 
   index = 0;
   prime = 3; 
   do {
@@ -46,42 +46,48 @@ int main (int argc, char *argv[])
     while (primes[++index]);  
     //printf("%d\n", prime);  
     prime = 2*index + 3;      
-  } while ((long long int)prime * prime <= sqn);
-  int *newprimes = (int *) malloc (psize*sizeof(int)); 
-  int nprime=0;
-  for (i = 0; i < psize; i++) {
-    if (!primes[i]) {
-      newprimes[nprime++]=2*i + 3;
+  } while (prime * prime <= sqn);
+  newprimes = (int *) malloc (psize*sizeof(int)); 
+  nprime=0;
+  for (j = 0; j < psize; j++) {
+    if (!primes[j]) {
+      newprimes[nprime++]=2*j + 3;
       //if (!id && nprime<100) printf("%d\n", newprimes[nprime-1]);
    }
   }
+  //printf("%d aaaaa %d\n", primes[14], psize);
 
-  marked = (char *) malloc (size);    
+  marked = (char *) calloc (size, sizeof(char));    
   if (marked == NULL) {    
     printf ("Cannot allocate enough memory\n");
     MPI_Finalize();
     exit (1);      
   }   
-  for (i = 0; i < size; i++) marked[i] = 0;  
-  
+  //if (id==7) printf("aaaa %d\n", high_value);
   for (k=0;k<size;k+=B) {
-    low_value = 3 + 2*(BLOCK_LOW(id,p,n/2-1)+k);
-    Bsize = MIN(B, (high_value-low_value)/2+1);
+    Blow_value = low_value+2*k;
+    //printf("%d  %d\n", k+B, size);
+    Bsize = MIN(k+B, size);
+    //for (i = 0; i < Bsize; i ++) printf("%d\n", i*2+Blow_value);  
     for (j = 0; j < nprime; j++) {
       prime = newprimes[j]; 
-      if (prime * prime > low_value)      
+      //if (k==0 && !id) printf("%d\n", prime);
+      if (prime * prime > Blow_value)      
       first = (prime * prime - low_value)/2;  
       else {  
-        if (!(low_value % prime)) first = 0;    
-        else first = prime - (low_value*(prime +1)/2 % prime);
+        if (!(Blow_value % prime)) first = k;    
+        else first = prime - (Blow_value*(prime +1)/2 % prime)+k;
       }
-      for (i = first; i < Bsize; i += prime) marked[i] = 1;    
+      for (i = first; i < Bsize; i += prime) { marked[i] = 1;  
+       // printf("%d\n", Blow_value+i*2);
+      } 
+      //if (!id) printf("first %d prime %d Bsize %d\n", first*2+low_value, prime, Bsize); 
     }
   }
   count = 0; 
   for (i = 0; i < size; i++)      
     if (!marked[i]) { count++;   
-//printf("%d\n",(i*2+low_value));
+      //printf("%d\n",(i*2+low_value));
     }
   MPI_Reduce (&count, &global_count, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);      
   elapsed_time += MPI_Wtime();    
